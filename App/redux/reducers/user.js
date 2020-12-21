0cb6edf2fd3d3a177d10c9db8ad76fabe2724bec
Andreas Nandiwardhana <andreasnw93@gmail.com>
2020-12-21 17:47:18 +0700

    add new package: immer

--- App/redux/reducers/user.js
+++ App/redux/reducers/user.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 const initialState = {
     user_data: [],
     active_user: {},
@@ -6,72 +8,63 @@ const initialState = {
 export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case 'ADD_USER':
-            return { ...state, user_data: state.user_data.concat(action.payload) };
+            return produce(state, draft => {
+                draft.user_data = draft.user_data.concat(action.payload);
+            });
         case 'SET_ACTIVE_USER':
             return { ...state, active_user: action.payload };
         case 'ADD_WISHLIST':
             let { item } = action.payload;
-            let { user_data, active_user } = state;
-            let tempUserData = JSON.parse(JSON.stringify(user_data));
-            let tempActiveUser = JSON.parse(JSON.stringify(active_user));
             let isDuplicate = false;
 
-            for (let i = 0; i < tempUserData.length; ++i) {
-                if (tempUserData[i].id === active_user.id) {
-                    for (let j = 0; j < tempUserData[i].list.length; ++j) {
-                        if (tempUserData[i].list[j].id === item.id) {
-                            isDuplicate = true;
-                            break;
+            return produce(state, draft => {
+                for (let i = 0; i < draft.user_data.length; ++i) {
+                    if (draft.user_data[i].id === draft.active_user.id) {
+                        for (let j = 0; j < draft.user_data[i].list.length; ++j) {
+                            if (draft.user_data[i].list[j].id === item.id) {
+                                isDuplicate = true;
+                                break;
+                            }
                         }
-                    }
 
-                    if (!isDuplicate) {
-                        tempUserData[i].list = tempUserData[i].list.concat(item);
-                        tempActiveUser.list = tempActiveUser.list.concat(item);
+                        if (!isDuplicate) {
+                            draft.user_data[i].list = draft.user_data[i].list.concat(item);
+                            draft.active_user.list = draft.active_user.list.concat(item);
+                        }
+                        break;
                     }
-                    break;
                 }
-            }
-
-            return { ...state, user_data: tempUserData, active_user: tempActiveUser };
+            })
         case 'EDIT_WISHLIST':
             item = action.payload;
-            user_data = state.user_data;
-            active_user = state.active_user;
-            tempUserData = JSON.parse(JSON.stringify(user_data));
-            tempActiveUser = JSON.parse(JSON.stringify(active_user));
 
-            for (let i = 0; i < tempUserData.length; ++i) {
-                if (tempUserData[i].id === active_user.id) {
-                    tempUserData[i].list = item;
-                    break;
-                }
-            };
-
-            tempActiveUser.list = item;
-
-            return { ...state, user_data: tempUserData, active_user: tempActiveUser };
+            return produce(state, draft => {
+                for (let i = 0; i < draft.user_data.length; ++i) {
+                    if (draft.user_data[i].id === draft.active_user.id) {
+                        draft.user_data[i].list = item;
+                        break;
+                    }
+                };
+    
+                draft.active_user.list = item;
+            })
         case 'REMOVE_WISHLIST':
             item = action.payload;
-            user_data = state.user_data;
-            active_user = state.active_user;
-            tempUserData = JSON.parse(JSON.stringify(user_data));
-            tempActiveUser = JSON.parse(JSON.stringify(active_user));
 
-            for (let i = 0; i < tempUserData.length; ++i) {
-                if (tempUserData[i].id === active_user.id) {
-                    for (let j = 0; j < tempUserData[i].list.length; j++) {
-                        if (tempUserData[i].list[j].id === item.id) {
-                            tempUserData[i].list.splice(j, 1);
-                            tempActiveUser.list.splice(j, 1);
-                            break;
+            return produce(state, draft => {
+                for (let i = 0; i < draft.user_data.length; ++i) {
+                    if (draft.user_data[i].id === draft.active_user.id) {
+                        for (let j = 0; j < draft.user_data[i].list.length; j++) {
+                            if (draft.user_data[i].list[j].id === item.id) {
+                                draft.user_data[i].list.splice(j, 1);
+                                draft.active_user.list.splice(j, 1);
+                                break;
+                            }
                         }
+                        break;
                     }
-                    break;
-                }
-            };
-
-            return { ...state, user_data: tempUserData, active_user: tempActiveUser };
+                };
+            })
         default:
             return state;
     }
